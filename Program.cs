1e637aa736411669d072fed39729342910f3f3e4
SafarovAT <13sasha13>
2015-07-04 14:04:22 +0500

    Ex

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1._1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Введите R1");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Введите R2");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("Введите R3");
+            float d = float.Parse(Console.ReadLine());
+            double R;
+            R = 1 / ((1 / a) + (1 / b) + (1 / d));
+            Console.WriteLine("Полное сопротивление: {0}", 1 / ((1 / a) + (1 / b) + (1 / d)));
+            Console.ReadKey();
+
+        }
+    }
+}
